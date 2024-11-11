08f99779fb9c8eab796f13002acb074ff6e5d4fa
jalilnawawi <ajnawawi@gmail.com>
2024-11-11 23:10:58 +0700

    revise : custom swagger url

--- src/main/java/com/jalil_be_app/madang_app/config/OpenApiConfig.java
+++ src/main/java/com/jalil_be_app/madang_app/config/OpenApiConfig.java
@@ -3,7 +3,14 @@ package com.jalil_be_app.madang_app.config;
 import io.swagger.v3.oas.annotations.enums.SecuritySchemeIn;
 import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
 import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.servers.Server;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
 
 @Configuration
 @SecurityScheme(
@@ -15,4 +22,18 @@ import org.springframework.context.annotation.Configuration;
         in = SecuritySchemeIn.HEADER
 )
 public class OpenApiConfig {
+    @Bean
+    public OpenAPI customOpenAPI(){
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Madang-App API Documentation")
+                        .version("1.0.0")
+                        .description("Documentation for Madang-App API using Springdoc OpenAPI and Swagger UI")
+                )
+                .servers(List.of(
+                        new Server().url("http://localhost:8080").description("HTTP Server"),
+                        new Server().url("https://madang-app.cloud").description("HTTPS Server")
+                ));
+
+    }
 }
