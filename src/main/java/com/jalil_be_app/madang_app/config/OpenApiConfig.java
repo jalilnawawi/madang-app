3f00085dd66d99008123bced9b3e2685fa4eb3f4
jalilnawawi <ajnawawi@gmail.com>
2024-11-12 00:17:15 +0700

    revise : setup openAPIDefinition

--- src/main/java/com/jalil_be_app/madang_app/config/OpenApiConfig.java
+++ src/main/java/com/jalil_be_app/madang_app/config/OpenApiConfig.java
@@ -1,18 +1,46 @@
 package com.jalil_be_app.madang_app.config;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.enums.SecuritySchemeIn;
 import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import io.swagger.v3.oas.annotations.servers.Server;
 import io.swagger.v3.oas.models.OpenAPI;
-import io.swagger.v3.oas.models.info.Info;
-import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import java.util.List;
 
-@Configuration
+@OpenAPIDefinition(
+        info = @Info(
+                contact = @Contact(
+                        name = "Madang-App API Support",
+                        email = "ajnawawi@gmail.com"
+                ),
+                title = "Madang-App API Documentation",
+                version = "1.0.0",
+                description = "OpenApi documentation for Madang-App API"
+        ),
+        servers = {
+                @Server(
+                        description = "Local",
+                        url = "http://localhost:8080"
+                ),
+                @io.swagger.v3.oas.annotations.servers.Server(
+                        description = "Production",
+                        url = "https://madang-app.cloud"
+                )
+        },
+        security = {
+                @SecurityRequirement(
+                        name = "bearerAuth"
+                )
+        }
+)
 @SecurityScheme(
         name = "bearerAuth",
         description = "JWT auth description",
@@ -22,18 +50,4 @@ import java.util.List;
         in = SecuritySchemeIn.HEADER
 )
 public class OpenApiConfig {
-    @Bean
-    public OpenAPI customOpenAPI(){
-        return new OpenAPI()
-                .info(new Info()
-                        .title("Madang-App API Documentation")
-                        .version("1.0.0")
-                        .description("Documentation for Madang-App API using Springdoc OpenAPI and Swagger UI")
-                )
-                .servers(List.of(
-                        new Server().url("http://localhost:8080").description("HTTP Server"),
-                        new Server().url("https://madang-app.cloud").description("HTTPS Server")
-                ));
-
-    }
 }
